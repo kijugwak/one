6775ad30860bdd989ad0435865d80433b815674b
kijugwak <82084502+kijugwak@users.noreply.github.com>
2021-04-08 17:06:48 +0900

    Create wikipedia.py

---
+++ wikipedia.py
@@ -0,0 +1,10 @@
+import wikipedia
+while True:
+  query = input("어떤게 궁금하신가요?")
+  
+  try:
+      results = wikipedia.summary(query, sentences =1)
+      print(results)
+      
+  except:
+    print("오류다 이색히야
